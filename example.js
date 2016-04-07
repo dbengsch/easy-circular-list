2361c51efdd9b85f0de1c1978af6ac8cd44a57ea
Anthony Sendra <asendra@kaliop.com>
2016-04-07 14:24:49 +0200

    update example

--- example.js
+++ example.js
@@ -1,16 +1,17 @@
 var CircularList = require("easy-circular-list");
 var list = new CircularList();
 
-list.add(1);
-list.add(2);
-list.add(3);
+list
+  .add(1)
+  .add(2)
+  .add(3);
 
 console.log(list.getNext());
 console.log(list.getNext());
 console.log(list.getNext());
 console.log(list.getNext());
 
-console.log(list.remove(2));
+list.remove(2);
 
 console.log(list.getNext());
 console.log(list.getNext());
